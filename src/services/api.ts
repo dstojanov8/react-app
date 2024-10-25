49d1e755cb71dd40560514e0e14f13ec83aeb131
Dimitrije <dstojanovga@gmail.com>
2024-10-25 15:58:57 +0200

    Port changed to the one docker container uses

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ import {
 } from "../types";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Set the base URL for all requests
+  baseURL: "http://127.0.0.1:8080", // Set the base URL for all requests
   withCredentials: true, // Send cookies with requests
   headers: {
     "Content-Type": "application/json",
